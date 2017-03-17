f208ce773faa2fe5da0f596a306def2149bbacd2
awemulya <awemulya@gmail.com>
2017-03-17 14:58:22 +0545

    code clean up

--- static/js/forms/setup.js
+++ static/js/forms/setup.js
@@ -48,6 +48,29 @@ var FieldSightXF = function (data){
 
 }
 
+var Schedule = function (data){
+  self = this;
+  self.id = ko.observable();
+  self.name = ko.observable();
+  self.form = ko.observable();
+  self.date_range_start = ko.observable();
+  self.date_range_end = ko.observable();
+  self.selected_days = ko.observableArray();
+  self.form_status = ko.observable();
+  self.is_deployed = ko.observable(false);
+
+  self.save = function(){
+    vm.scheduleVm().saveSchedule(self)
+    vm.generalVm().general_form_modal_visibility(false);
+  };
+  
+  for (var i in data){
+    self[i] = ko.observable(data[i]);
+}
+
+}  
+
+
 var GeneralVM = function(is_project, pk){
   var self = this;
   self.pk = pk;
@@ -108,9 +131,10 @@ var GeneralVM = function(is_project, pk){
 
             };
     var failure =  function (errorThrown) {
+      var err_message = errorThrown.responseJSON.non_field_errors;
                 App.hideProcessing();
                 App.notifyUser(
-                        'Duplicate Form',
+                        err_message,
                         'error'
                     );
 
@@ -133,6 +157,35 @@ var GeneralVM = function(is_project, pk){
     }
     });
 }
+var ScheduleVM = function(is_project, pk){
+  var self = this;
+  self.pk = pk;
+  self.is_project = is_project;
+  self.label = "Schedules";
+  self.allGForms = ko.observableArray();
+  self.forms = ko.observableArray();
+  self.current_form = ko.observable();
+  self.schedule_form_modal_visibility = ko.observable(false);
+  self.search_key = ko.observable();
+
+  self.add_form = function(){
+    self.current_form(new Schedule());
+    self.schedule_form_modal_visibility(true);
+  };
+
+
+
+self.search_key.subscribe(function (newValue) {
+    if (!newValue) {
+        self.forms(self.allGForms());
+    } else {
+        filter_forms = ko.utils.arrayFilter(self.allGForms(), function(item) {
+            return ko.utils.stringStartsWith(item.name.toLowerCase(), newValue);
+        });
+        self.forms(filter_forms);
+    }
+    });
+}
 
 
 function SetUpViewModel(is_project, pk) {
@@ -141,7 +194,7 @@ function SetUpViewModel(is_project, pk) {
   self.pk = pk;
   self.currentVm = ko.observable("general");
   self.generalVm = ko.observable();
-  self.schedulesVm = ko.observable();
+  self.scheduleVm = ko.observable();
   self.stagesVm = ko.observable();
   self.generalVm(new GeneralVM(is_project, pk));
 
@@ -152,12 +205,19 @@ function SetUpViewModel(is_project, pk) {
   
   self.currentVm.subscribe(function (newValue) {
     if(newValue == "general" ) {
+      if (ko.utils.unwrapObservable(self.generalVm()) == null){
+        self.generalVm(new GeneralVM(is_project, pk));
+      }
         
     } else if (newValue == "schedules") {
-      self.schedulesVm(new GeneralVM(is_project, "schedules"));
+      if (ko.utils.unwrapObservable(self.scheduleVm()) == null){
+        self.scheduleVm(new ScheduleVM(is_project, pk));
+      }
 
     }else if (newValue == "stages") {
-      self.stagesVm(new GeneralVM(is_project, "stages"));
+     if (ko.utils.unwrapObservable(self.stagesVm()) == null){
+        self.stagesVm(new GeneralVM(is_project, pk));
+      }
 
     }
     });
