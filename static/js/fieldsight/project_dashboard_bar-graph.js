a61b574f52681ec8f512185c13318d97d5c7e2f0
awemulya <awemulya@gmail.com>
2017-04-09 10:10:41 +0545

    project dashboard bar

--- static/js/fieldsight/project_dashboard_bar-graph.js
+++ static/js/fieldsight/project_dashboard_bar-graph.js
@@ -33,62 +33,34 @@ var svg = d3.select("#progress-bar").append("svg")
 
 svg.call(tip);
 
-// The new data variable.
-var data = [
-  {letter: "10-20", frequency: 10},
-  {letter: "B", frequency: 20},
-  {letter: "C", frequency: 30},
-  {letter: "D", frequency: 30},
-  {letter: "E", frequency: 30},
-  {letter: "F", frequency: 30},
-  {letter: "G", frequency: 30},
-  {letter: "H", frequency: 30},
-  {letter: "I", frequency: 30},
-  {letter: "J", frequency: 30},
-  {letter: "K", frequency: 30},
-  {letter: "L", frequency: 100},
-  {letter: "M", frequency: .02517},
-  {letter: "N", frequency: .06749},
-  {letter: "O", frequency: .07507},
-  {letter: "P", frequency: .01929},
-  {letter: "Q", frequency: .00098},
-  {letter: "R", frequency: .05987},
-  {letter: "S", frequency: .06333},
-  {letter: "T", frequency: .09056},
-  {letter: "U", frequency: .02758},
-  {letter: "V", frequency: .01037},
-  {letter: "W", frequency: .02465},
-  {letter: "X", frequency: .00150},
-  {letter: "Y", frequency: .01971},
-  {letter: "Z", frequency: .00074}
-];
 
-// The following code was contained in the callback function.
-x.domain(data.map(function(d) { return d.letter; }));
-y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+x.domain(dict_bar_data.map(function(d) { return d.letter; }));
+y.domain([0, d3.max(dict_bar_data, function(d) { return d.frequency; })]);
 
 svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis)
     .append("text")
-    .attr("text-anchor", "end")
-    .attr("y", 30)
-    .attr("x", 920)
-    .text("Progress");
+    .attr("y", 20)
+    .attr("x",500)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Progress %");
+
 
 svg.append("g")
     .attr("class", "y axis")
     .call(yAxis)
   .append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 10)
-    .attr("dx", ".71em")
+    .attr("y", -20)
+    .attr("dy", ".71em")
     .style("text-anchor", "end")
-    .text("No Of Sites");
+    .text("Sites");
 
 svg.selectAll(".bar")
-    .data(data)
+    .data(dict_bar_data)
   .enter().append("rect")
     .attr("class", "bar")
     .attr("x", function(d) { return x(d.letter); })
