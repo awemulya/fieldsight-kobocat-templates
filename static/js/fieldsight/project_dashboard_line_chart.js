85baf4ee515199799088f669a9129d23fc286069
awemulya <awemulya@gmail.com>
2017-04-10 10:39:36 +0545

    line chart

--- static/js/fieldsight/project_dashboard_line_chart.js
+++ static/js/fieldsight/project_dashboard_line_chart.js
@@ -4,16 +4,16 @@ var margin = {
     bottom: 30,
     left: 50
 };
-var width = 600 - margin.left - margin.right;
-var height = 270 - margin.top - margin.bottom;
+var width = 800 - margin.left - margin.right;
+var height = 370 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%d-%b-%y").parse;
+var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 var x = d3.time.scale().range([0, width]);
 var y = d3.scale.linear().range([height, 0]);
 
 var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+    .orient("bottom").ticks(10);
 
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(5);
@@ -34,38 +34,22 @@ var svg = d3.select("#submission-chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Get the data
-var data = [{
-    date: "1-May-12",
-    close: "58.13"
-}, {
-    date: "30-Apr-12",
-    close: "53.98"
-}, {
-    date: "27-Apr-12",
-    close: "67.00"
-}, {
-    date: "26-Apr-12",
-    close: "89.70"
-}, {
-    date: "25-Apr-12",
-    close: "99.00"
-}];
 
-data.forEach(function (d) {
+line_chart_data.forEach(function (d) {
     d.date = parseDate(d.date);
     d.close = +d.close;
 });
 
 // Scale the range of the data
-x.domain(d3.extent(data, function (d) {
+x.domain(d3.extent(line_chart_data, function (d) {
     return d.date;
     }));
-y.domain([0, d3.max(data, function (d) {
+y.domain([0, d3.max(line_chart_data, function (d) {
     return d.close;
     })]);
 
 svg.append("path") // Add the valueline path.
-.attr("d", valueline(data));
+.attr("d", valueline(line_chart_data));
 
 svg.append("g") // Add the X Axis
 .attr("class", "x axis")
