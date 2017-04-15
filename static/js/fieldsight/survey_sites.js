40f8a55fdf81b0d2b9f408209557fb5a098aef54
awemulya <awemulya@gmail.com>
2017-04-15 17:14:42 +0545

    merged

--- static/js/fieldsight/survey_sites.js
+++ static/js/fieldsight/survey_sites.js
@@ -1,8 +1,21 @@
+  var SiteType = function(data) {
+  	var self = this;
+        this.id = ko.observable();
+        this.name = ko.observable();
+    
+	
+	for (var i in data){
+       self[i] = ko.observable(data[i]);
+              }
+
+    };
+
 var Site =function (data){
   self = this;
   self.id = ko.observable();
   self.identifier = ko.observable();
   self.name = ko.observable();
+  
   self.type = ko.observable();
   self.address = ko.observable();
   self.phone = ko.observable();
@@ -12,33 +25,46 @@ var Site =function (data){
   self.logo = ko.observable();
   self.is_active = ko.observable();
   self.location = ko.observable();
+  self.latitude = ko.observable();
+  self.longitude = ko.observable();
   self.is_survey = ko.observable();
 
   self.save = function(){
     vm.site_modal_visibility(false);
   };
+ 
   for (var i in data){
     self[i] = ko.observable(data[i]);
-              }
+  }
 
   self.mapOne = ko.observable({
-        lat: ko.observable(27.714875814507074),
-        lng: ko.observable(85.3243088722229)});
-}
-
+        lat: ko.observable(self.latitude()),
+        lng: ko.observable(self.longitude())});
+};
 
 
 function SitesViewModel(project) {
   var self=this;
   self.project = project;
+  self.current_site = ko.observable();
+  self.site_modal_visibility = ko.observable(false);
   self.allSites = ko.observableArray();
   self.sites = ko.observableArray();
+  self.typeList =  ko.observableArray();
+  
+  self.detail_survey = function(site){
+  	self.current_site(site);
+  	self.site_modal_visibility(true);
+  };
 
+  self.close_detail = function(){
+  	self.site_modal_visibility(false);
+  };
 
   self.loadSites = function(){
     App.showProcessing();
         $.ajax({
-            url: '/fieldsight/api/survey-sites/'+self.project+'/',
+            url: '/fieldsight/api/survey-sites-review/'+self.project+'/',
             method: 'GET',
             dataType: 'json',
             // data: post_data,
@@ -60,7 +86,32 @@ function SitesViewModel(project) {
         });
   };
 
+  self.loadTypes = function(){
+    App.showProcessing();
+        $.ajax({
+            url: '/fieldsight/api/project-types/',
+            method: 'GET',
+            dataType: 'json',
+            // data: post_data,
+            // async: true,
+            success: function (response) {
+                App.hideProcessing();
+	               var mappedTypeData = ko.utils.arrayMap(response, function(item) {
+                        return new SiteType(item);
+                    });
 
+                self.typeList(mappedTypeData);
+
+            },
+            error: function (errorThrown) {
+                App.hideProcessing();
+                console.log(errorThrown);
+            }
+        });
+  };
+
+
+  self.loadTypes();
   self.loadSites();
   
   self.search_key = ko.observable();
