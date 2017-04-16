2f24a0ce4a1411e01dbfa589f9cbc8c81999ee3e
awemulya <awemulya@gmail.com>
2017-04-16 17:13:26 +0545

    survey site

--- static/js/fieldsight/survey_sites.js
+++ static/js/fieldsight/survey_sites.js
@@ -15,7 +15,7 @@ var Site =function (data){
   self.id = ko.observable();
   self.identifier = ko.observable();
   self.name = ko.observable();
-  
+  self.create_surveys = ko.observableArray();
   self.type = ko.observable();
   self.address = ko.observable();
   self.phone = ko.observable();
@@ -28,6 +28,7 @@ var Site =function (data){
   self.latitude = ko.observable();
   self.longitude = ko.observable();
   self.is_survey = ko.observable();
+  self.mapOne = ko.observable();
 
   self.save = function(){
     vm.site_modal_visibility(false);
@@ -36,10 +37,11 @@ var Site =function (data){
   for (var i in data){
     self[i] = ko.observable(data[i]);
   }
+  self.mapOne({'lat':ko.observable(self.latitude()),'lng':ko.observable(self.longitude())});
+  
+  self.type(new SiteType({'id':self.type().id,'name':self.type().name}));
+  self.is_survey(false);
 
-  self.mapOne = ko.observable({
-        lat: ko.observable(self.latitude()),
-        lng: ko.observable(self.longitude())});
 };
 
 
@@ -57,8 +59,52 @@ function SitesViewModel(project) {
   	self.site_modal_visibility(true);
   };
 
-  self.close_detail = function(){
-  	self.site_modal_visibility(false);
+  self.update_survey = function(update_site){
+  App.showProcessing();
+  var site = new Site({'type':{'id':update_site.type().id(),'name':update_site.type().name()}});
+  site.id = update_site.id();
+  site.identifier = update_site.identifier();
+  site.name = update_site.name();
+  site.public_desc = update_site.public_desc();
+  site.additional_desc = update_site.additional_desc();
+  site.latitude = update_site.mapOne().lat();
+  site.longitude = update_site.mapOne().lng();
+  site.type = update_site.type().id();
+  site.is_survey = update_site.is_survey();
+
+var url = '/fieldsight/api/survey-sites-review-update/'+site.id+'/';
+var success =  function (response) {
+                App.hideProcessing();
+                self.site_modal_visibility(false);
+                self.loadSites();
+                App.notifyUser(
+                        'Site Survey Saved',
+                        'success'
+                    );
+
+            };
+    
+var failure =  function (errorThrown) {
+      var err_message = errorThrown.responseJSON.non_field_errors;
+      if (err_message==undefined){
+        err_message = "Failed To Save Data"
+      }
+                App.hideProcessing();
+                App.notifyUser(
+                        err_message,
+                        'error'
+                    );
+
+            };
+
+    App.remotePost(url, site, success, failure);                                                                                                                    
+  
+  };
+
+
+  self.save_detail = function(){
+    self.update_survey(self.current_site());
+  	
   };
 
   self.loadSites = function(){
@@ -112,6 +158,7 @@ function SitesViewModel(project) {
 
 
   self.loadTypes();
+
   self.loadSites();
   
   self.search_key = ko.observable();
