f7da925aa1161536f5b5ce053350b3363119317e
awemulya <awemulya@gmail.com>
2017-07-14 13:42:31 +0545

    map datas

--- static/js/map.js
+++ static/js/map.js
@@ -31,12 +31,38 @@ var baseLayers = {
 
 markers = new L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
-        icon = L.icon({
-                        //iconSize: [27, 27],
-                        iconAnchor: [13, 27],
-                        popupAnchor:  [1, -24],
-                        iconUrl: static_url+'css/images/marker-icon.png'
-                        });
+        if(feature.status == 0){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/red-small.png'
+                    });
+                }
+                else if(feature.status == 1){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/blue-small.png'
+                    });
+                }
+                else if(feature.status == 2){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/green-small.png'
+                    });
+                }
+                else if(feature.status == 3){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/orange-small.png'
+                    });
+                }
         //console.log(icon.options);
         var marker = L.marker(latlng, {icon: icon});
         return marker;
@@ -68,9 +94,125 @@ markers = new L.geoJson(data, {
 
 // map.fitBounds(markers.getBounds());
 
-        layerswitcher.addOverlay(markers, "Schools");
+        // layerswitcher.addOverlay(markers, "Schools");
+
+        progressLayer = new L.geoJson(data, {
+            pointToLayer: function(feature, latlng) {
+                // console.log(feature);
+                if(feature.progress <= 20){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/green-small.png'
+                    });
+                }
+                else if(feature.progress >= 20 && feature.progress <= 40){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/gray-small.png'
+                    });
+                }
+                else if(feature.progress >= 40 && feature.progress <= 60){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/yellow-small.png'
+                    });
+                }
+                else if(feature.progress >= 60 && feature.progress <= 80){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/black-small.png'
+                    });
+                }
+                else if(feature.progress >= 80 && feature.progress <= 100){
+                    icon = L.icon({
+                                //iconSize: [27, 27],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/violet-small.png'
+                    });
+                }
+                //console.log(icon.options);
+                var marker = L.marker(latlng, {icon: icon});
+                return marker;
+                
+            }, 
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(feature.properties.public_desc+'<br/>'+feature.properties.name+'<br/>'+feature.properties.address);
+                
+            }
+        });
+        
+        
+        //layerswitcher.addOverlay(progressLayer, "Progress");
+        
+        $(".switch").on('change',function(){
+            /* if($(this).is(':checked') == false){
+                $(this).is(':checked') = true;
+            }
+            else{
+                $(this).is(':checked') = false;
+            } */
+            $('.switch').not(this).prop('checked', false);
+            if($(this)[0].id == "form_status"){
+                if(map.hasLayer(progressLayer)){
+                    console.log('here');
+                    map.removeLayer(progressLayer);
+                }
+                
+                if(!map.hasLayer(markers)){
+                    console.log('not geojson layer');
+                    map.addLayer(markers);
+                }
+            }
+            else if($(this)[0].id == "project_progress"){
+                if(map.hasLayer(markers)){
+                    
+                    map.removeLayer(markers);
+                }
+                if(!map.hasLayer(progressLayer)){
+                    map.addLayer(progressLayer);
+            }
+            }
+        });
 
 markers.on('click',function(e){
+    var site_id = e.layer.feature.id;
+    // console.log(server_url+'/fieldsight/api/site-schedules/'+site_id);
+    $.ajax({
+url: '/fieldsight/api/site-images/'+site_id,
+type: 'get', // This is the default though, you don't actually need to always mention it
+dataType: "json",
+success: function(data) {
+img = '';
+console.log(data.images);
+for(i=0;i<data.images.length;i++){console.log(data.images[i]);
+    img += '<img src = '+data.images[i]+'/>';
+}
+
+ $('.popop-container').show();
+ $('.popop-gallery').html(img);
+//$('.popop-gallery').html(data.images);
+},
+failure: function(data) { 
+}
+}); 
+    $('.popop-head').addClass('changed');
+    $('.popop-head').html(e.layer.feature.properties.name);
+    $('.address-popop').html(e.layer.feature.properties.address);
+    //$('.contact-popup').html(e.layer.feature.properties.contact);
+
+
+});
+
+progressLayer.on('click',function(e){
     var site_id = e.layer.feature.id;
     // console.log(server_url+'/fieldsight/api/site-schedules/'+site_id);
     $.ajax({
