2340d33082d9b2fd53d4fcf43de89d116f478676
narayanmagar <thmgrnarayan@gmail.com>
2017-09-19 10:55:58 +0545

    new template change

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,3 @@
-
-
 var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
@@ -29,50 +27,57 @@ var baseLayers = {
     "Google Terrain": googleTerrain
 };
 
+//arrays for legend
+statusIconList = ['marker-red','marker-yellow','marker-green','marker-blue','marker-grey'];
+statusText = ['rejected','flagged','approved','pending','not received'];
+progressIconList = ['marker-red','marker10-20','marker30-40','marker50-60','marker70-80','marker80-90','marker90-100'];
+progressText = ['0%','0-20%','20-40%','40-60%','60-80%','80-100%', '100%'];
+//end array legend
+
 markers = new L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
         // rejected
-        if(feature.status == 3){
+        if(feature.status == 1){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/red-small.png'
+                                iconUrl: static_url+'images/marker-red.png'
                     });
                 }
                 else if(feature.status == 2){
                     // flagged
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/yellow-small.png'
+                                iconUrl: static_url+'images/marker-yellow.png'
                     });
                 }
-                else if(feature.status == 1){
+                else if(feature.status == 3){
                     // approved
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/green-small.png'
+                                iconUrl: static_url+'images/marker-green.png'
                     });
                 }
                 else if(feature.status == 0){
                     // pending
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/orange-small.png'
+                                iconUrl: static_url+'images/marker-blue.png'
                     });
                 }else if(feature.status == 4){
-                    // pending
+                    // pending ( No Data )
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/blue-small.png'
+                                iconUrl: static_url+'images/marker-grey.png'
                     });
                 }
         //console.log(icon.options);
@@ -108,47 +113,67 @@ markers = new L.geoJson(data, {
 
         // layerswitcher.addOverlay(markers, "Schools");
 
+
+
+
         progressLayer = new L.geoJson(data, {
             pointToLayer: function(feature, latlng) {
                 // console.log(feature);
-                if(feature.progress <= 20){
+                if(feature.progress == 0){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/red-small.png'
+                                iconUrl: static_url+'images/marker-red.png'
                     });
                 }
-                else if(feature.progress >= 20 && feature.progress <= 40){
+                else if(feature.progress >= 1 && feature.progress <= 20){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/violet-small.png'
+                                iconUrl: static_url+'images/marker10-20.png'
                     });
                 }
-                else if(feature.progress >= 40 && feature.progress <= 60){
+                
+                else if(feature.progress > 20 && feature.progress <= 40){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/orange-small.png'
+                                iconUrl: static_url+'images/marker30-40.png'
                     });
                 }
-                else if(feature.progress >= 60 && feature.progress <= 80){
+                else if(feature.progress > 40 && feature.progress <= 60){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/yellow-small.png'
+                                iconUrl: static_url+'images/marker50-60.png'
                     });
                 }
-                else if(feature.progress >= 80 && feature.progress <= 100){
+                else if(feature.progress > 60 && feature.progress <= 80){
                     icon = L.icon({
-                                //iconSize: [27, 27],
+                                iconSize: [40, 40],
                                 iconAnchor: [13, 27],
                                 popupAnchor:  [1, -24],
-                                iconUrl: static_url+'images/green-small.png'
+                                iconUrl: static_url+'images/marker70-80.png'
+                    });
+                }
+                else if(feature.progress > 80 && feature.progress < 100){
+                    icon = L.icon({
+                                iconSize: [40, 40],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/marker80-90.png'
+                    });
+                }
+                else if(feature.progress == 100){
+                    icon = L.icon({
+                                iconSize: [40, 40],
+                                iconAnchor: [13, 27],
+                                popupAnchor:  [1, -24],
+                                iconUrl: static_url+'images/marker90-100.png'
                     });
                 }
                 //console.log(icon.options);
@@ -166,7 +191,48 @@ markers = new L.geoJson(data, {
         
         
         //layerswitcher.addOverlay(progressLayer, "Progress");
-        
+        //legend start
+       function addLegend(type){
+
+       var legend = L.control({position: 'bottomright'});  
+                        legend.onAdd = function (map) {
+                        $('.leaflet-control').remove();
+                        var div = L.DomUtil.create('div', 'info legend'),
+                            labels = [''];
+                            //console.log(div);
+                        if(type == 'form_status'){
+		                labels.push('<strong>Form Status</strong>');
+		                for (var i = 0; i < statusIconList.length; i++) {
+		                    
+		                //console.log(icons[i]);
+		                labels.push(
+		                    '<div style="display: inline-block; width:25px; height: 25px; "><img style="display: inline-block; width:15px; height: 20px;" src = "'+static_url+'images/'+statusIconList[i]+'.png"></img></div> &ndash; '+ statusText[i]);
+		                    }
+		                    div.innerHTML = labels.join('<br/>');
+		                    return div;
+		                
+                        }
+                        else {
+                                labels.push('<strong>Site Progress</strong>');
+		                for (var i = 0; i < progressIconList.length; i++) {
+		                    
+		                //console.log(icons[i]);
+		                labels.push(
+		                    '<div style="display: inline-block; width:25px; height: 25px; "><img style="display: inline-block; width:15px; height: 20px;" src = "'+static_url+'images/'+progressIconList[i]+'.png"></img></div> &ndash; '+ progressText[i]);
+		                    }
+		                    div.innerHTML = labels.join('<br/>');
+		                    return div;
+		                
+                        }
+                        };
+                        legend.addTo(map);
+                        $(".info.legend.leaflet-control").css({'background-color':'white', 'padding': '5px'});
+                    }
+          addLegend('form_status');
+
+//legend end
+
+
         $(".switch").on('change',function(){
             /* if($(this).is(':checked') == false){
                 $(this).is(':checked') = true;
@@ -176,6 +242,7 @@ markers = new L.geoJson(data, {
             } */
             $('.switch').not(this).prop('checked', false);
             if($(this)[0].id == "form_status"){
+                addLegend('form_status');
                 if(map.hasLayer(progressLayer)){
                     console.log('here');
                     map.removeLayer(progressLayer);
@@ -187,6 +254,7 @@ markers = new L.geoJson(data, {
                 }
             }
             else if($(this)[0].id == "project_progress"){
+                addLegend('project_progress');
                 if(map.hasLayer(markers)){
                     
                     map.removeLayer(markers);
@@ -261,5 +329,3 @@ map.on('click',function(){
     $('.address-popop').html('');
     //$('.contact-popup').html('');
 });
-
-
