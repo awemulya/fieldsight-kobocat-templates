c57c15948734c838dd1b8494bc7eb485a5b5c6dd
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-10-28 19:10:27 +0545

    notification js fixed for type 18 and 19

--- static/js/notificationhandler.js
+++ static/js/notificationhandler.js
@@ -117,12 +117,12 @@
         }   
 
     function type18(data){
-               content = '<b><a href="' + data.get_source_url + '">'+ data.source_name +'</a></b> assigned a new '+ data.extra_message +' in project <b><a href="' + data.get_event_url+ '">' + data.get_event_name+'</a></b>';
+               content = '<b><a href="' + data.get_source_url + '">'+ data.source_name +'</a></b> assigned a new <b><a href="' + data.get_event_url+ '">' + data.get_event_name+'</a></b> in project <b><a href="' + data.get_extraobj_url+ '">' + data.get_extraobj_name+'</a></b>';
                   return content;
         }   
 
     function type19(data){
-               content = '<b><a href="' + data.get_source_url + '">'+ data.source_name +'</a></b> assigned a new '+ data.extra_message +' in site <b><a href="' + data.get_event_url+ '">' + data.get_event_name+'</a></b>';
+               content = '<b><a href="' + data.get_source_url + '">'+ data.source_name +'</a></b> assigned a new <b><a href="' + data.get_event_url+ '">' + data.get_event_name+'</a></b> in site <b><a href="' + data.get_extraobj_url+ '">' + data.get_extraobj_name+'</a></b>';
                   return content;
         }   
 
