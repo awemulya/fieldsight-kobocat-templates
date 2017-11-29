3669aac8c37a9f12413eb9e02abc6a012b3721e4
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-29 14:03:05 +0545

    region js added.

---
+++ static/js/fieldsight/project_regions.js
@@ -0,0 +1,65 @@
+var Region =function (data){
+  self = this;
+  self.id = ko.observable();
+  self.name = ko.observable();
+  self.url= ko.observable("/fieldsight/regional-site-list/"+self.id()+"/");
+  for (var i in data){
+    self[i] = ko.observable(data[i]);
+      }
+}
+
+
+
+function RegionViewModel(project) {
+  var self=this;
+  self.allRegions = ko.observableArray();
+  self.regions = ko.observableArray();
+  
+  
+  self.loadRegion = function(){
+    App.showProcessing();
+        $.ajax({
+            url: '/fieldsight/api/project-regions/'+project+'/',
+            method: 'GET',
+            dataType: 'json',
+            // data: post_data,
+            // async: true,
+            success: function (response) {
+                App.hideProcessing();
+               var mappedData = ko.utils.arrayMap(response, function(item) {
+                        datas = new Region(item);
+                        console.log(datas);
+                        return datas;
+                    });
+                self.allRegions(mappedData);
+                self.regions(mappedData);
+
+            },
+            error: function (errorThrown) {
+                App.hideProcessing();
+                console.log(errorThrown);
+            }
+        });
+  };
+
+
+  self.loadRegion();
+
+  self.clearRegion = function(){
+  self.region_add_visibility(false);
+    self.current_region(undefined);
+  };
+  self.search_key = ko.observable();
+  self.search_key.subscribe(function (newValue) {
+    if (!newValue) {
+        self.regions(self.allRegions());
+    } else {
+      newValue = newValue.toLowerCase();
+        filter_regions = ko.utils.arrayFilter(self.allRegions(), function(item) {
+            return (ko.utils.stringStartsWith(item.name().toLowerCase(), newValue) || 
+              ko.utils.stringStartsWith(item.address().toLowerCase(), newValue));
+        });
+        self.regions(filter_regions);
+    }
+    });
+};
