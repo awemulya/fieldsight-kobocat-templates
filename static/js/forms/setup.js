adceb8d8f6892082c08f45fb4ee9fbb123a9875e
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-01-11 16:09:50 +0545

    fixes in setup.js

--- static/js/forms/setup.js
+++ static/js/forms/setup.js
@@ -474,7 +474,7 @@ console.log(self.stage_forms().xf);
 self.default_submission_status_text = ko.observable(formStatus(self.stage_forms().default_submission_status));
 
 self.default_submission_status_text.subscribe(function (newValue) { 
-console.log(formStatus(self.stage_forms().default_submission_status));
+console.log(self.stage_forms().default_submission_status);
 console.log(self.stage_forms().id());
 
     doAssignDefaultFormStatus(self.stage_forms().id(), newValue);
