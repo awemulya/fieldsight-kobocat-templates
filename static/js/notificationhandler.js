7916065bd501c14cc2f4f775f2b5a248b3f3fb5a
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-01-21 12:07:40 +0545

    notification time issue fixed.

--- static/js/notificationhandler.js
+++ static/js/notificationhandler.js
@@ -239,7 +239,7 @@ function dateparser(date){
        }
      }
     month = d.getMonth();
-    time=d.toString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC' });
+    time=d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC' });
     clean_date = months[month] +', '+ d.getDate() +', '+ d.getFullYear() +', '+ time.toLowerCase()+'.';
     console.log(d.getUTCDate() +"/ "+ d.getUTCHours() +"/ "+d.getUTCMinutes())+"/ ";
     return clean_date;
