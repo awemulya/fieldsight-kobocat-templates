68befae272a6ab8cb572289b1b95ee6758933159
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-26 12:33:40 +0545

    fix in js

--- static/js/fieldsight/manage_project_dashboard.js
+++ static/js/fieldsight/manage_project_dashboard.js
@@ -321,12 +321,13 @@ window.app = new Vue({
                   </div>
                   </template>
                   <template v-else>
-                    <span>No Data.</span>
+                    <span>{{ status }}</span>
                   </template>
                 </div>
               </div>
             </div>`,
   data: {
+        status: "Loading"
         headers: [],
         sub_headers :[],
         rows :[],
@@ -353,6 +354,9 @@ window.app = new Vue({
         self.sub_headers = response.body.content.sub_stages;
         self.rows = response.body.content.rows;
         self.loading = false;
+        if (response.body.content.sub_stages.length == 0){
+          status="No Data";
+        }
     }
 
     function errorCallback() {
